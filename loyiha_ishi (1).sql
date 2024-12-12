a3988f4c200cb78356d0438198fddda38623196e
Oybekahmadaliyev1 <oybekahmadaliyev6@gmail.com>
2024-12-12 17:09:03 +0500

    Add files via upload

---
+++ loyiha_ishi (1).sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1
+-- Время создания: Дек 12 2024 г., 13:10
+-- Версия сервера: 10.4.32-MariaDB
+-- Версия PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `loyiha_ishi_oybek`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `loyiha_ishi`
+--
+
+CREATE TABLE `loyiha_ishi` (
+  `id` int(11) NOT NULL,
+  `FIO` varchar(40) NOT NULL,
+  `Passport_seria_raqam` varchar(50) NOT NULL,
+  `Manzil` varchar(50) NOT NULL,
+  `Ariza_berilgan_sana` varchar(13) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
